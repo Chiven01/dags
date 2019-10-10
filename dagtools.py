05125ca1679611bd6beb9cd1b0c03f35000c3197
Chiven01 <chiven001@gmail.com>
2019-10-10 12:39:16 +0800

    add lots of dags

--- dagtools.py
+++ dagtools.py
@@ -14,9 +14,23 @@ def alter(file):
     with open(file,"w",encoding="utf-8") as f:
         f.write(file_data)
 
-file_path = sys.argv[1]
-if file_path is None or len(file_path) == 0:
-    print('there is no file_path')
-    sys.exit()
-# file_path = '..\\datas\\dag-example'
-alter(file_path)
+def copydag(num=1):
+    file='thour10.py'
+    for i in range(num):
+        new_str = 'thour%s' % (10+1+i)
+        newFile = new_str + '.py'
+        print("new_str=%s, newFile=%s" % (new_str, newFile))
+        file_data = ""
+        with open(file, "r", encoding="utf-8") as f:
+            for line in f:
+                line = re.sub(r'thour10', new_str ,line)
+                file_data += line
+        with open(newFile,"w",encoding="utf-8") as f:
+            f.write(file_data)
+
+#file_path = sys.argv[1]
+#if file_path is None or len(file_path) == 0:
+#    print('there is no file_path')
+#    sys.exit()
+#alter(file_path)
+copydag(50)
---
+++ temp29.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+u"""
+### 文档
+这里可以写上此产品的长篇描述，
+使用markdown语法书写哦~
+"""
+
+from airflow import DAG
+from airflow import utils
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime,timedelta
+
+default_args = {
+    'owner': 'chenxianxin',
+    'email': ['chenxianxin@sogou-inc.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'execution_timeout': timedelta(hours=12),
+    'depends_on_past': True,
+    'start_date': utils.dates.days_ago(3)
+}
+
+dag = DAG(
+    'temp29',
+    default_args = default_args,
+    description = u'信息流业务统计',
+    schedule_interval = "@daily"
+)
+dag.doc_md = __doc__
+
+env = {'HIVE_CONF_DIR':'daohang','HADOOP_CONF_DIR':'daohang'}
+
+lens = [140,5,5,2,2]
+
+temps = []
+for n in range(lens[0]):
+    temp = 'temp_%s' % n
+    temp = BashOperator(
+    dag = dag,
+    env = env,
+    task_id = temp,
+    svn_url = 'http://svn.sogou-inc.com/svn/sogouime/DataAnalysis/platform_java/imedaapptask/newsfeed/temp',
+    bash_command ='sleep 120',
+    schedule_interval = '0 5 * * *',
+    doc_md = u"""""")
+    temps.append(temp)
+
+taemps = []
+for n in range(lens[1]):
+    taemp = 'taemp_%s' % n
+    taemp = BashOperator(
+    dag = dag,
+    env = env,
+    task_id = taemp,
+    svn_url = 'http://svn.sogou-inc.com/svn/sogouime/DataAnalysis/platform_java/imedaapptask/newsfeed/taemp',
+    bash_command ='sleep 120',
+    schedule_interval = '0 5 * * *',
+    doc_md = u"""""")
+    taemps.append(taemp)
+
+tbemps = []
+for n in range(lens[2]):
+    tbemp = 'tbemp_%s' % n
+    tbemp = BashOperator(
+    dag = dag,
+    env = env,
+    task_id = tbemp,
+    svn_url = 'http://svn.sogou-inc.com/svn/sogouime/DataAnalysis/platform_java/imedaapptask/newsfeed/tbemp',
+    bash_command ='sleep 120',
+    schedule_interval = '0 5 * * *',
+    doc_md = u"""""")
+    tbemps.append(tbemp)
+
+tcemps = []
+for n in range(lens[3]):
+    tcemp = 'tcemp_%s' % n
+    tcemp = BashOperator(
+    dag = dag,
+    env = env,
+    task_id = tcemp,
+    svn_url = 'http://svn.sogou-inc.com/svn/sogouime/DataAnalysis/platform_java/imedaapptask/newsfeed/tcemp',
+    bash_command ='sleep 120',
+    schedule_interval = '0 5 * * *',
+    doc_md = u"""""")
+    tcemps.append(tcemp)
+
+tdemps = []
+for n in range(lens[4]):
+    tdemp = 'tdemp_%s' % n
+    tdemp = BashOperator(
+    dag = dag,
+    env = env,
+    task_id = tdemp,
+    svn_url = 'http://svn.sogou-inc.com/svn/sogouime/DataAnalysis/platform_java/imedaapptask/newsfeed/tdemp',
+    bash_command ='sleep 120',
+    schedule_interval = '0 5 * * *',
+    doc_md = u"""""")
+    tdemps.append(tdemp)
+
+for te in temps:
+    for ta in taemps:
+        for tb in tbemps:
+            for tc in tcemps:
+                for td in tdemps:
+                    ta >> te
+                    tb >> ta
+                    tc >> tb
+                    td >> tc
+
+#for te in temps:
+#    for ta in taemps:
+#        ta >> te
+
